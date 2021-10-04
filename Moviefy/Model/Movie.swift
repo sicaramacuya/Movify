0e337debbcdefebaa41d9663581699b279e456ae
sicaramacuya <eric.morales@protonmail.com>
2021-10-04 13:26:09 -0400

    create codable model

--- Moviefy/Model/Movie.swift
+++ Moviefy/Model/Movie.swift
@@ -15,3 +15,43 @@ struct Movie {
     let releaseDate: String
 }
 
+// Handle Pagination
+struct MovieApiResponse {
+    let page: Int
+    let numberOfPages: Int
+    let movies: [Movie]
+}
+
+extension Movie: Codable {
+    enum MovieCodingKeys: String, CodingKey {
+        case id
+        case title
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let movieContainer = try decoder.container(keyedBy: MovieCodingKeys.self)
+        
+        id = try movieContainer.decode(Int.self, forKey: .id)
+        title = try movieContainer.decode(String.self, forKey: .title)
+        posterPath = try movieContainer.decode(String.self, forKey: .posterPath)
+        releaseDate = try movieContainer.decode(String.self, forKey: .releaseDate)
+    }
+}
+
+extension MovieApiResponse: Codable {
+    private enum MovieApiResponseCodingKeys: String, CodingKey {
+        case page
+        case numberOfPages = "total_pages"
+        case movies = "results"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: MovieApiResponseCodingKeys.self)
+        
+        page = try container.decode(Int.self, forKey: .page)
+        numberOfPages = try container.decode(Int.self, forKey: .numberOfPages)
+        movies = try container.decode([Movie].self, forKey: .movies)
+    }
+}
